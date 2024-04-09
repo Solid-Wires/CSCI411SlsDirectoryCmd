4809f4398739b38ea01f98b5f409d3361197f851
Solid Wires <solid.wires@gmail.com>
2024-04-09 13:26:54 -0400

    filesystem not supported from this version, doing what I should do instead

--- inc/common.h
+++ inc/common.h
@@ -3,7 +3,6 @@
 // Includes
 #include <iostream>
 #include <fstream>
-#include <filesystem>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <dirent.h>
--- src/main.cc
+++ src/main.cc
@@ -4,21 +4,27 @@ using namespace std;
 // Entry
 //  Param 1: Directory to check (defaults to working directory)
 int main(int argc, char *argv[]) {
+
+    // Stat
+    struct stat sb;
+
     // Directory to check
-    string targetDir = get_current_dir_name(); // from unistd.h
+    const char* targetDir; // from unistd.h
+
     if (argc > 1) {
         // (optional) Manually provide the target directory from param
         targetDir = argv[1];
 
         // Check if this is a directory
-        if (!filesystem.exists(targetDir)) {
+        if (stat(targetDir, sb) == 0) {
             cout << "The given target directory does not exist." << endl;
             return -1;
         }
         cout << "Provided target dir: " << targetDir << endl;
     }
     else {
-        // Print the current working directory if param not provided
+        // Use the current working directory if param not provided
+        targetDir = get_current_dir_name();
         cout << "Current working dir: " << targetDir << endl;
     }
 
