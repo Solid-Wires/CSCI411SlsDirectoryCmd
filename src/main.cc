23e2e58a9ca8bfbf1bd920d04a2234ce93a738d2
Solid Wires <solid.wires@gmail.com>
2024-04-09 13:39:10 -0400

    Moved this to after path retrieval

--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,7 @@ using namespace std;
 //  Param 1: Directory to check (defaults to working directory)
 int main(int argc, char *argv[]) {
 
-    // Stat (path properties)
+    // Stat (path stats)
     struct stat sb;
 
     // Directory to check
@@ -15,18 +15,17 @@ int main(int argc, char *argv[]) {
     if (argc > 1) {
         // (optional) Manually provide the target directory from param
         targetDir = argv[1];
-
-        // Check if this path exists
-        if (stat(targetDir, &sb) == -1) {
-            cout << "The given target directory does not exist." << endl;
-            return -1;
-        }
         cout << "Provided target dir: " << targetDir << endl;
     }
     else {
         // Use the current working directory if param not provided
         targetDir = get_current_dir_name(); // from unistd.h
         cout << "Current working dir: " << targetDir << endl;
+    }
+    // Check if this path exists (and retrieve path stats)
+    if (stat(targetDir, &sb) == -1) {
+        cout << "The given target directory does not exist." << endl;
+        return -1;
     }
 
     // Check if this path is a directory
