8ff9365da28b1c86d240ee63020f5192644ed438
Solid Wires <solid.wires@gmail.com>
2024-04-09 23:53:53 -0400

    Nevermind

--- src/main.cc
+++ src/main.cc
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
                 //  print out the string stream.
                 stringstream ss;
                 WriteDirContents(targetDir, &ss);
-                cout << setw(4) << ss.str();
+                cout << ss.str();
                 break;
             }
         default: // Anything
