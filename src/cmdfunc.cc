9ca03140bbaa3a5d6c3c4ab8355d5adb3623cd43
Solid Wires <solid.wires@gmail.com>
2024-04-09 22:16:14 -0400

    There we go

--- src/cmdfunc.cc
+++ src/cmdfunc.cc
@@ -20,7 +20,7 @@ void WriteDirContents(const char* path, stringstream *ss) {
         string targetPath((string)path + '/' + dp->d_name); // Stat requires a path to the file. Luckily, we already kind of know it here
         struct stat sb;
         stat(targetPath.c_str(), &sb); // Could use lstat, but the example seems to just use stat
-        *ss << TagDirectory(dp->d_type) << '\t' << dp->d_name << '\t' << sb.st_size << '\t' << sb.st_mtime << endl;
+        *ss << TagDirectory(dp->d_type) << '\t' << dp->d_name << '\t' << sb.st_size << '\t' << ctime(&sb.st_mtime) << endl;
     }
     // Done listing the dir
     closedir(dir);
