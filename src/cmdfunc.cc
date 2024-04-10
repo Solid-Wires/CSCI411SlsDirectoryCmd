e06fd78c895ca668b8159bd4e23981b22d3f3333
Solid Wires <solid.wires@gmail.com>
2024-04-09 23:02:27 -0400

    Perm flags made

--- src/cmdfunc.cc
+++ src/cmdfunc.cc
@@ -41,8 +41,42 @@ const char* TagDirectory(unsigned char d_type) {
 string DisplayPerms(mode_t st_mode) {
     // The output string is 9 characters long.
     char permFlags[10] = "---------"; 
-    //switch(st_mode & SR_IRWXU) {
-    //    case 
-    //}
+    
+    // Check permissions for user/owner
+    switch(st_mode & S_IRWXU) {
+        case S_IRUSR:
+            permFlags[0] = 'r';
+            break;
+        case S_IWUSR:
+            permFlags[1] = 'w';
+            break;
+        case S_IXUSR:
+            permFlags[2] = 'x';
+            break;
+    }
+    // Check permissions for group
+    switch(st_mode & S_IRWXG) {
+        case S_IRGRP:
+            permFlags[3] = 'r';
+            break;
+        case S_IWGRP:
+            permFlags[4] = 'w';
+            break;
+        case S_IXGRP:
+            permFlags[5] = 'x';
+            break;
+    }
+    // Check permissions for others
+    switch(st_mode & S_IRWXO) {
+        case S_IROTH:
+            permFlags[6] = 'r';
+            break;
+        case S_IWOTH:
+            permFlags[7] = 'w';
+            break;
+        case S_IXOTH:
+            permFlags[8] = 'x';
+            break;
+    }
     return (string)permFlags;
 }
